d2d4c3300b807508744a40b9bfb4517bb180040f
Ivan Itzcovich <i.itzcovich@gmail.com>
2018-08-22 18:01:37 -0300

    Fix paths to models

--- faced/const.py
+++ faced/const.py
@@ -1,6 +1,6 @@
 import os
 
-MODELS_PATH = os.path.join(os.path.dirname(__file__), '..', "models")
+MODELS_PATH = os.path.join(os.path.dirname(__file__), "models")
 
 YOLO_SIZE = 288
 YOLO_TARGET = 9
